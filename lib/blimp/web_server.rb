4aab03a842af852db21335d85633613dfe9a09f6
Marten Veldthuis <marten@veldthuis.com>
2011-12-29 20:27:09 +0100

    remove hardcoded path

--- lib/blimp/web_server.rb
+++ lib/blimp/web_server.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 module Blimp
   class WebServer < Sinatra::Base
     before do
-      @root = "/home/marten/prj/blimp/sample"
+      @root = File.join(File.expand_path(File.dirname(__FILE__)), "../../sample")
       @redis = Redis.new
       FileSystemSyncer.new(@root, @redis).sync!
     end
