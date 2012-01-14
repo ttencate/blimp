79fee6bff54360c5b1c97ba7bdea46fa7ff04e3e
Marten Veldthuis <marten@veldthuis.com>
2012-01-14 21:11:34 +0100

    Add a little logging to webserver

--- lib/blimp/web_server.rb
+++ lib/blimp/web_server.rb
@@ -7,10 +7,14 @@ module Blimp
     end
 
     before do
-      logger.info "Sites defined: #{Site.all}"
+      logger.info "Started GET \"#{request.path_info}\" for #{request.ip} at #{Time.now.strftime("%Y-%m-%d %H:%M:%s")}"
       @site = Site.find_by_domain(request.host) or raise Sinatra::NotFound, "No site defined for #{request.host}"
     end
-    
+
+    after do
+      logger.info ""
+    end
+
     get '*' do
       begin
         page = @site.find_page(request.path_info)
