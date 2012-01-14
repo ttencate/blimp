6939b8c2e67606a35d565bc5e0786e86aa0fc885
Thomas ten Cate <ttencate@gmail.com>
2012-01-14 20:38:17 +0000

    Make quotes consistentish

--- spec/blimp/renderer_spec.rb
+++ spec/blimp/renderer_spec.rb
@@ -10,7 +10,7 @@ describe Blimp::Renderer do
       Blimp::Renderer.render("*markdown*", "text/markdown").should == ["<p><em>markdown</em></p>\n", "text/html"]
     end
 
-    it "raises on unknown MIME types" do
+    it 'raises on unknown MIME types' do
       expect {
         Blimp::Renderer.render("text", "text/plain")
       }.to raise_error(Blimp::Renderer::UnknownType)
