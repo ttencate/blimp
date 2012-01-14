7c965e7952720c8c9f633f55ade63c5ca26d3d55
Marten Veldthuis <marten@veldthuis.com>
2012-01-14 20:29:58 +0100

    Sites have domains, and can be stored

--- lib/blimp/site.rb
+++ lib/blimp/site.rb
@@ -4,12 +4,33 @@ class Site
   attr_reader :key
   attr_reader :source
   attr_reader :theme
+  attr_reader :domains
 
   liquid_methods :title
 
-  def initialize(key, source)
+  class << self
+    protected
+    attr_accessor :sites
+
+    public
+    def add(site)
+      self.sites ||= []
+      self.sites << site
+    end
+
+    def all
+      self.sites
+    end
+    
+    def find_by_domain(domain)
+      Site.all.find {|i| i.has_domain?(domain) }
+    end
+  end
+
+  def initialize(key, source, options = {})
     @key = key
     @source = source
+    @domains = options[:domains] || []
 
     @theme = Theme.new(source, "/templates")
   end
@@ -20,6 +41,10 @@ class Site
 
   def render_page(page)
     theme.render_page(page)
+  end
+
+  def has_domain?(domain)
+    domains.include?(domain)
   end
 
 end
--- spec/blimp/site_spec.rb
+++ spec/blimp/site_spec.rb
@@ -10,6 +10,22 @@ describe Site do
     site.should be
   end
 
+  describe ".add" do
+    it 'remembers a site' do
+      Site.add(site)
+      Site.all.should == [site]
+    end
+  end
+
+  describe ".find_by_domain" do
+    it 'finds a site with a single domain' do
+      domain = "example.com"
+      site = Site.new("my-site", source, domains: [domain])
+      Site.add(site)
+      Site.find_by_domain(domain).should == site
+    end
+  end
+
   describe "config" do
     it "should have a config if file exists"
     it "should have an empty config if file does not exist"
@@ -29,6 +45,30 @@ describe Site do
       page = stub
       page.stub!(:body).and_return("Page contents")
       site.render_page(page).should include("Page contents")
+    end
+  end
+
+  describe "#domains" do
+    it "returns the domains" do
+      domains = ["test"]
+      site = Site.new("my-site", source, :domains => domains)
+      site.domains.should == domains
+    end
+
+    it "returns an empty array if none given" do
+      Site.new("my-site", source).domains.should == []
+    end
+  end
+
+  describe "#has_domain?" do
+    let(:site) { Site.new("my-site", source, domains: ["example.com"]) }
+
+    it "returns true if the domain is literally in the domains list" do
+      site.has_domain?("example.com").should be_true   
+    end
+
+    it "is not true for unspecified domains" do
+      site.has_domain?("google.com").should be_false
     end
   end
 end
