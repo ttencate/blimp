9b5c4129b71dcc796f812940bba781709635ba74
Marten Veldthuis <marten@veldthuis.com>
2012-01-14 22:17:25 +0100

    fix that webserver specs need to raise exceptions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+ENV["RACK_ENV"] = "test"
+
+# Start code coverage as the first thing
 require "simplecov"
 SimpleCov.start
+
 require 'rspec'
 require 'mock_redis'
 require 'fakefs/spec_helpers'
 require 'blimp'
+
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
