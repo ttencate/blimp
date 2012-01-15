0c46387b32e9ae62505d6a730629ee668f3cfd8a
Marten Veldthuis <marten@veldthuis.com>
2012-01-15 21:43:48 +0100

    dont symlink if none defined

--- config/deploy/symlinks.rb
+++ config/deploy/symlinks.rb
@@ -1,13 +1,15 @@
 namespace :symlinks do
   desc "Make all the damn symlinks"
   task :make, :roles => :app, :except => { :no_release => true } do
-    commands = shared_symlinks.map do |from, to|
-      "ln -sf #{shared_path}/#{from} #{current_path}/#{to}"
-    end
+    if shared_symlinks.any?
+      commands = shared_symlinks.map do |from, to|
+        "ln -sf #{shared_path}/#{from} #{current_path}/#{to}"
+      end
 
-    run <<-CMD
-      cd #{current_path} && #{commands.join(" && ")}
-    CMD
+      run <<-CMD
+        cd #{current_path} && #{commands.join(" && ")}
+      CMD
+    end
   end
 end
 
