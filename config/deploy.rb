db64c07f38550e20d9464638744184b8a872f14c
Marten Veldthuis <marten@veldthuis.com>
2012-01-15 21:13:27 +0100

    update blimp deploy location

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :repository,  "git://github.com/ttencate/blimp.git"
 set :branch,      "origin/master"
-set :deploy_to,   "/var/www/blimp.martenveldthuis.com"
+set :deploy_to,   "/var/www/blimp"
 set :current_path,"#{deploy_to}/current"
 set :shared_path, "#{deploy_to}/shared"
 set :user,        "blimp"
